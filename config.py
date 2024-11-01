a21cc6bbb9a431b81f2baae59d7a891dfeaee6ad
manoranjan23 <manoranjanhor43@gmail.com>
2024-11-01 11:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID","24740695"))
-API_HASH = getenv("API_HASH","a95990848f2b93b8131a4a7491d97092")
+API_ID = int(getenv("API_ID","28588693"))
+API_HASH = getenv("API_HASH","fac94f1f1aa4aa395280a670ddf9c0f2")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN","7739226387:AAEePjJcFpB2PLZZCcRPP6Vf8eHbZJHFSGo")
 # Add Owner Username without @ 
