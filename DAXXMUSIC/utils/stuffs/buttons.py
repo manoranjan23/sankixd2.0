41ed995c5065682230649f61464b1f13e67a24db
manoranjan23 <manoranjanhor43@gmail.com>
2024-11-02 20:42:33 +0530

    Update buttons.py

--- DAXXMUSIC/utils/stuffs/buttons.py
+++ DAXXMUSIC/utils/stuffs/buttons.py
@@ -1,1 +1,11 @@
-gj
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+# BUTTONS ka basic structure
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton("Play", callback_data="play_callback")],
+        [InlineKeyboardButton("Pause", callback_data="pause_callback")],
+        [InlineKeyboardButton("Stop", callback_data="stop_callback")],
+        [InlineKeyboardButton("Skip", callback_data="skip_callback")],
+    ]
+)
