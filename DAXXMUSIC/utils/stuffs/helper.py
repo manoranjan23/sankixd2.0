1098987f19fb039d83ae3cca670872fe8b2d1703
manoranjan23 <manoranjanhor43@gmail.com>
2024-11-02 20:48:26 +0530

    Update stuffs

---
+++ DAXXMUSIC/utils/stuffs/helper.py
@@ -0,0 +1,6 @@
+class Helper:
+    @staticmethod
+    def example_method():
+        return "This is an example method from the Helper class."
+
+    # Aap yahan aur methods add kar sakte hain jo aapko chahiyein
